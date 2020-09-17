0d8d6523162297794611ed45e779455c78ab697d
dsouzaj98 <66758205+dsouzaj98@users.noreply.github.com>
2020-09-17 15:54:46 -0400

    add recommender

--- src/recommender.py
+++ src/recommender.py
@@ -1,4 +1,5 @@
 from eda import *
+import numpy as np
 
 content_data = books_data[['original_title','authors','average_rating']]
 content_data = content_data.astype(str)
@@ -34,3 +35,53 @@ def get_recommendations_books(title, cosine_sim=cosine_sim_author):
 def author_book_shows(book):
     for book in book:
         print(book)
+
+merge_data=merge_data[:40000]
+book_rating=pd.pivot_table(merge_data, index='user_id', values='rating', columns='book_title', fill_value=0)
+
+book_corr=np.corrcoef(book_rating.T)
+book_list=list(book_rating)
+book_titles=[]
+for i in range(len(book_list)):
+    book_titles.append(book_list[i])
+
+def get_reco_collab(books_list):
+    similar_books=np.zeros(book_corr.shape[0])
+    for book in books_list:
+        book_index=book_titles.index(book)
+        similar_books +=book_corr[book_index]
+    book_prefs=[]
+    for i in range(len(book_titles)):
+        book_prefs.append((book_titles[i], similar_books[i]))
+    return sorted(book_prefs, key=lambda x:x[1], reverse=True)
+
+
+list_of_books = ['one hundred years of solitude',
+                 'stardust',
+                 'mogs christmas',
+                 'dragonmede',
+                 'twopence to cross the mersey',
+                 'the candywine development']
+def get_final_reco(list1):
+    test=get_reco_collab(list1)
+    sim_books=[]
+    i=0
+    for n in range(10):
+        sim_book=test[i][0]
+        i+=1
+        if sim_book in list1:
+            continue
+        else:
+            sim_books.append(sim_book)
+    return sim_books
+    # i=0
+    # n=0
+    # while n<9:
+    #     similar_books_to_read= test[i][0]
+    #     i += 1
+    #     if similar_books_to_read in list_of_books:
+    #         continue
+    #     else:
+
+    #         print(similar_books_to_read)
+    #         n += 1
