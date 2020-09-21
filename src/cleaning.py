6ff708e6c7b555f3f53c9c2f560a995e0daaadad
dsouzaj98 <66758205+dsouzaj98@users.noreply.github.com>
2020-09-21 15:10:56 -0400

    edit clustering

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,10 +1,10 @@
 import pandas as pd 
 
 #goodbooks data
-books_data=pd.read_csv("data/goodbooks/books.csv")
-tags_data=pd.read_csv('data/goodbooks/book_tags.csv')
-ratings_data=pd.read_csv('data/goodbooks/ratings.csv')
-book_tags=pd.read_csv('data/goodbooks/tags.csv')
+books_data=pd.read_csv("data/goodbooks-10k-data/books.csv")
+tags_data=pd.read_csv('data/goodbooks-10k-data/book_tags.csv')
+ratings_data=pd.read_csv('data/goodbooks-10k-data/ratings.csv')
+book_tags=pd.read_csv('data/goodbooks-10k-data/tags.csv')
 
 #book crossing data
 u_cols=['user_id','location','age']
@@ -17,21 +17,21 @@ r_cols=['user_id', 'isbn', 'rating']
 cross_ratings_data=pd.read_csv('data/Book_reviews/BX-Book-Ratings.csv', sep=';', names=r_cols, encoding='latin-1', low_memory=False, skiprows=1)
 
 #drop unnecessary columns
-books_data = books_data.drop(columns=['id', 'best_book_id', 'work_id', 'isbn', 'isbn13', 'title','work_ratings_count',
-                                   'work_text_reviews_count', 'ratings_1', 'ratings_2', 'ratings_3', 'ratings_4', 'ratings_5', 
-                                    'image_url','small_image_url'])
+# books_data = books_data.drop(columns=['id', 'best_book_id', 'work_id', 'isbn', 'isbn13', 'title','work_ratings_count',
+#                                    'work_text_reviews_count', 'ratings_1', 'ratings_2', 'ratings_3', 'ratings_4', 'ratings_5', 
+#                                     'image_url','small_image_url'])
 
-books_data = books_data.dropna()
-cross_books_data = cross_books_data.drop(columns=['img_s', 'img_m', 'img_1'])
+# books_data = books_data.dropna()
+# cross_books_data = cross_books_data.drop(columns=['img_s', 'img_m', 'img_1'])
 
 #remove duplicates
-ratings_data = ratings_data.sort_values("user_id")
-ratings_data.drop_duplicates(subset =["user_id","book_id"], keep = False, inplace = True) 
-books_data.drop_duplicates(subset='original_title',keep=False,inplace=True)
-book_tags.drop_duplicates(subset='tag_id',keep=False,inplace=True)
-tags_data.drop_duplicates(subset=['tag_id','goodreads_book_id'],keep=False,inplace=True)
-cross_ratings_data.drop_duplicates(subset =["user_id","isbn"], keep = False, inplace = True) 
-cross_books_data.drop_duplicates(subset='book_title',keep=False,inplace=True)
+# ratings_data = ratings_data.sort_values("user_id")
+# ratings_data.drop_duplicates(subset =["user_id","book_id"], keep = False, inplace = True) 
+# books_data.drop_duplicates(subset='original_title',keep=False,inplace=True)
+# book_tags.drop_duplicates(subset='tag_id',keep=False,inplace=True)
+# tags_data.drop_duplicates(subset=['tag_id','goodreads_book_id'],keep=False,inplace=True)
+# cross_ratings_data.drop_duplicates(subset =["user_id","isbn"], keep = False, inplace = True) 
+# cross_books_data.drop_duplicates(subset='book_title',keep=False,inplace=True)
 
 
 
